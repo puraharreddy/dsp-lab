121ae016d34b83b25148ad33ef3dfe2fef667ee5
puraharreddy <70189779+puraharreddy@users.noreply.github.com>
2020-11-17 10:58:15 +0530

    Add files via upload

---
+++ implement selection sort(iterative).py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 09:45:07 2020
+
+@author: sony
+"""
+#purahar reddy , 22
+#implementing selection sort(iterative) 
+arr=[]
+n=int(input("enter range:"))
+for i in range(n):
+    ele=int(input())
+    arr.append(ele)
+#code begins
+for i in range(len(arr)):
+    min_indx=i
+    for j in range(i+1,len(arr)):
+        if arr[min_indx]>arr[j]:
+            min_indx=j
+#swap
+    arr[i],arr[min_indx]=arr[min_indx],arr[i]
+#print
+print("Sorted array is :")
+for i in range(len(arr)):
+    print(arr[i])
